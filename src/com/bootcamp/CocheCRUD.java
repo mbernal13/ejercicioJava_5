3371840f57fddc54cea78c4245c702a4cc911e05
Miguel Bernal Ramirez <mbernal@gcp.com>
2022-11-10 17:58:15 -0500

    ejercicio 5

---
+++ src/com/bootcamp/CocheCRUD.java
@@ -0,0 +1,7 @@
+package com.bootcamp;
+
+public interface CocheCRUD {
+    void save();
+    void findAll();
+    void delete();
+}
---
+++ src/com/bootcamp/Main.java
@@ -0,0 +1,11 @@
+package com.bootcamp;
+
+public class Main {
+
+    public static void main (String[] args){
+        CocheCRUD coche = new CocheCRUDImpl();
+        coche.save();
+        coche.findAll();
+        coche.delete();
+    }
+}
